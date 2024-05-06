3a37db0b1fef5659fafe5577f799ce1efd0038d1
nlemuel <lemuelayres@gmail.com>
2024-05-06 17:07:08 -0300

    feat: update user.js

--- src/data/user.js
+++ src/data/user.js
@@ -13,15 +13,15 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Desenvolvedor Web no final de sua jornada na busca de seu diploma de Análise e desenvolvimento de sistemas",
+		title: "Desenvolvedor no final de sua jornada na busca de seu diploma de Análise e desenvolvimento de sistemas",
 		description:
-			"Aluno da FATEC apaixonado por front-end desde o terceiro semestre do curso, aprendendo sobre Javascript e Typescript. Gosto de resolver problemas, aprender habilidades novas e as vezes crio alguns problemas também. Procuro por oportunidades para aprimorar minhas habilidades como desenvolvedor web.",
+			"Aluno da FATEC apaixonado por dados e front-end desde o terceiro semestre do curso, aprendendo sobre Typescript, IA e Python. Gosto de resolver problemas, aprender habilidades novas e as vezes crio alguns problemas também. Procuro por oportunidades para aprimorar minhas habilidades como desenvolvedor web.",
 	},
 
 	about: {
 		title: "Me chamo Nicolas Lemuel. Moro em São José dos Campos, cidade conhecida por estar a 1 hora de qualquer cidade do Brasil, aqui é onde estou construindo minha base para dar grandes saltos no futuro.",
 		description:
-			"Minha primeira experiência profissional na área de TI foi como estagiário na Vivo Telefônica, lá aprendi muito não apenas sobre habilidades técnicas mas também sobre as soft skills necessárias para um bom trabalho em equipe. Passei por alguns desafios onde apenas o conhecimento técnico não foi o sufiente para chegarmos a entrega final. Sai de lá com uma grnade bagagem de aprendizado e com muita vontade de aplicá-lo, atualmente estou engajando em comunidades open source para contribuir com projetos front-end adquirindo mais experiência.",
+			"Minha primeira experiência profissional na área de TI foi como estagiário na Vivo Telefônica, lá aprendi muito não apenas sobre habilidades técnicas mas também sobre as soft skills necessárias para um bom trabalho em equipe. Passei por alguns desafios onde apenas o conhecimento técnico não foi o sufiente para chegarmos a entrega final. Sai de lá com uma grnade bagagem de aprendizado e com muita vontade de aplicá-lo, atualmente estou engajando em comunidades open source para contribuir com projetos adquirindo mais experiência.",
 	},
 
 	articles: {
@@ -32,12 +32,12 @@ const INFO = {
 
 	projects: [
 		{
-			title: "Ensinio",
+			title: "Altave",
 			description:
-				"Teste técnico para a Ensinio, plataforma de cursos online",
-			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+				"Projeto de visão computacional, ainda em desenvolvimento, realizado com a Altave pela FATEC",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
 			linkText: "View Project",
-			link: "https://github.com/nlemuel/hero-section",
+			link: "https://github.com/CodeLabFatec/ProjetoIA",
 		},
 
 		{
@@ -58,14 +58,14 @@ const INFO = {
 			link: "https://github.com/PowerTech5API/iacity",
 		},
 
-		// {
-		// 	title: "Project 4",
-		// 	description:
-		// 		"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
-		// 	logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
-		// 	linkText: "View Project",
-		// 	link: "https://github.com",
-		// },
+		{
+			title: "Ensinio",
+			description:
+				"Teste técnico para a Ensinio, plataforma de cursos online",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/nlemuel/hero-section",
+		},
 
 		// {
 		// 	title: "Project 5",
